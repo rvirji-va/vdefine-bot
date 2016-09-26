87e8e8f75d3a1098115384cf34cf7903d94be11c
rvirji-va <rvirji@vendasta.com>
2016-09-26 13:35:22 -0600

    adds direct message feature to vdefine

--- python/vdefine.py
+++ python/vdefine.py
@@ -5,11 +5,12 @@ import json
 from slackclient import SlackClient
 
 # starterbot's ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
+BOT_ID = 'U2FCRRL74'
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
 DEFINE = ('define', 'what is', 'explain', 'wtf is')
+
 IDENTIFY = ('identify', 'who is')
 HELP = ('help', 'who are you', 'what are you', 'explain')
 
@@ -60,12 +61,16 @@ def parse_slack_output(slack_rtm_output):
 		directed at the Bot, based on its ID.
 	"""
 	output_list = slack_rtm_output
+
 	if output_list and len(output_list) > 0:
 		for output in output_list:
 			if output and 'text' in output and AT_BOT in output['text']:
 				# return text after the @ mention, whitespace removed
 				return output['text'].split(AT_BOT)[1].strip().lower(), \
 					   output['channel']
+			elif output and 'text' in output and output['channel'] in _get_dm_ids() and not output['user'] == BOT_ID:
+				# direct message
+				return output['text'].strip().lower(), output['channel']
 	return None, None
 
 def _get_definition(query):
@@ -94,6 +99,12 @@ def _retrieve_query_from_input(input, commands_to_strip):
 		if input.startswith(item):
 			return input.replace(item, "", 1).strip()
 
+def _get_dm_ids():
+	im_list = slack_client.api_call("im.list")
+	bot_ims = []
+	for im in im_list["ims"]:
+		bot_ims = bot_ims + [im["id"]]
+	return tuple(bot_ims)
 
 if __name__ == "__main__":
 	READ_WEBSOCKET_DELAY = 0.5 # 1 second delay between reading from firehose
